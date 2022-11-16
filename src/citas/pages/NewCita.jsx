937cd8c00886440dbe72b99e7314a828dfe31c9a
carlos <carloscruz1dev@gmail.com>
2022-11-16 09:17:27 -0600

    validation

--- src/citas/pages/NewCita.jsx
+++ src/citas/pages/NewCita.jsx
@@ -1,18 +1,23 @@
+//others
+import { Formik } from 'formik';
+
 //icons 
 import { FiX } from 'react-icons/fi'
 
 //hook
 import { useNewCita } from '../hooks/useNewCita'
 
+//components
+import { Message } from '../../components/Message';
+import { Loading } from '../../components/Loading';
+
 //css
 import '../css/NewCita.css'
-import { Loading } from '../../components/Loading';
-import { Message } from '../../components/Message';
 
 export const NewCita = ({ handleNewCita }) => {
     const { data, isLoading, message, formState, handleNewAppointment, onInputChange } = useNewCita();
     const { namePatient, lastNamePatient, emailPatient, phonePatient, descriptionPatient, appointmentTime, dateAppointment } = formState
-    
+
     return (
         <>
             {
@@ -22,95 +27,159 @@ export const NewCita = ({ handleNewCita }) => {
                 <FiX className='newCita__icon' onClick={handleNewCita} />
                 <div className="newCita__content" >
                     <h2 className="citas__pendientes">Nueva cita</h2>
-                    <form className="newCita__form">
-                        <div className="newCita__data">
-                            <label>Nombre:</label>
-                            <input
-                                type="text"
-                                name='namePatient'
-                                onChange={onInputChange}
-                                placeholder='Escribe tu nombre'
-                                value={namePatient}
-                            />
-                        </div>
-                        <div className="newCita__data">
-                            <label>Apellido:</label>
-                            <input
-                                type="text"
-                                name='lastNamePatient'
-                                onChange={onInputChange}
-                                placeholder='Escribe tu apellido'
-                                value={lastNamePatient}
-                            />
-                        </div>
-                        <div className="newCita__data">
-                            <label>email:</label>
-                            <input
-                                type="email"
-                                name='emailPatient'
-                                onChange={onInputChange}
-                                placeholder='Dijita tu email'
-                                value={emailPatient}
-                            />
-                        </div>
-                        <div className="newCita__data">
-                            <label>Numero de telefono:</label>
-                            <input
-                                type="number"
-                                name='phonePatient'
-                                onChange={onInputChange}
-                                placeholder='Dijita tu numero de telefono'
-                                value={phonePatient}
-                            />
-                        </div>
-                        <div className='newCita__date'>
-                            <div className="newCita__data newCita__data--day">
-                                <label>Elige el dia</label>
-                                <input
-                                    type="date"
-                                    name='dateAppointment'
-                                    onChange={onInputChange}
-                                    value={dateAppointment}
-                                />
-                            </div>
-                            <div className="newCita__data newCita__data--time">
-                                <label>Elige la hora</label>
-                                <input
-                                    type="time"
-                                    name='appointmentTime'
-                                    list="listalimitestiempo"
-                                    step="600"
-                                    onChange={onInputChange}
-                                    value={appointmentTime}
-                                />
-                                <datalist id="listalimitestiempo">
-                                    <option value="08:00"></option>
-                                    <option value="09:00"></option>
-                                    <option value="10:00"></option>
-                                    <option value="03:00"></option>
-                                </datalist>
-                            </div>
-                        </div>
-                        <div className="newCita__data">
-                            <label>Descripción de los sintomas:</label>
-                            <textarea
-                                name="descriptionPatient"
-                                id="descriptio"
-                                placeholder='Escribe una breve descripción'
-                                onChange={onInputChange}
-                                value={descriptionPatient}
-                            />
-                        </div>
+
+                    <Formik
+
+                        initialValues={{
+                            name: '',
+                            lastname: '',
+                            email: JSON.parse(localStorage.getItem('user')).email,
+                            phone: '',
+                            date: '',
+                            time: '',
+                            description: '',
+
+                        }}
+
+                        validate={(values) => {
+                            let errors = {};
+                            //name
+                            if (!values.name) {
+                                errors.nombre = 'Dijita tu nombre '
+                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name))
+                                errors.nombre = 'El nombre solo puede contener letras y espacios'
+                            //lastname
+                            if (!values.lastname) {
+                                errors.lastname = 'Eijita tu apellido '
+                            } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.lastname))
+                                errors.lastname = 'El apellido solo puede contener letras y espacios'
+                            //email
+                            if (!values.email) {
+                                errors.email = 'Dijita tu correo '
+                            } else if (!/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(values.email))
+                                errors.email = 'Dijita un correo valido'
+                            //phone
+                            if (!values.phone) {
+                                errors.phone = 'Dijita tu numero de telefono '
+                            } else if (!/^[0-9]/.test(values.phone))
+                                errors.email = 'numero de telefono no valido'
+
+                            return errors;
+                        }}
+
+                        onSubmit={() => {
+                            console.log("formilario enviado");
+                        }}
+                    >
                         {
-                            message.status && <Message status={data?.Status} message={message} className='login__message' /> 
+                            ({ handleSubmit, handleChange, handleBlur, touched, values, errors }) => (
+                                <form className="newCita__form" onSubmit={handleSubmit}>
+                                    <div className="newCita__data">
+                                        <label>Nombre:</label>
+                                        <input
+                                            type="text"
+                                            name='name'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            placeholder='Escribe tu nombre'
+                                            value={values.name}
+                                        />
+                                        {touched && errors.nombre && <p>{errors.nombre}</p>}
+                                    </div>
+
+                                    <div className="newCita__data">
+                                        <label>Apellido:</label>
+                                        <input
+                                            type="text"
+                                            name='lastname'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            placeholder='Escribe tu apellido'
+                                            value={values.lastname}
+                                        />
+                                        {touched.lastname && errors.lastname && <p>{errors.lastname}</p>}
+                                    </div>
+
+                                    <div className="newCita__data">
+                                        <label>email:</label>
+                                        <input
+                                            type="email"
+                                            name='email'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            placeholder='Dijita tu email'
+                                            value={values.email}
+                                        />
+                                        {touched.email && errors.email && <p>{errors.email}</p>}
+                                    </div>
+
+                                    <div className="newCita__data">
+                                        <label>Numero de telefono:</label>
+                                        <input
+                                            type="number"
+                                            name='phone'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            placeholder='Dijita tu numero de telefono'
+                                            value={values.phone}
+                                        />
+                                        {touched.phone && errors.phone && <p>{errors.phone}</p>}
+                                    </div>
+                                    <div className='newCita__date'>
+                                        <div className="newCita__data newCita__data--day">
+                                            <label>Elige el dia</label>
+                                            <input
+                                                type="date"
+                                                name='date'
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.date}
+                                            />
+                                        </div>
+                                        <div className="newCita__data newCita__data--time">
+                                            <label>Elige la hora</label>
+                                            <input
+                                                type="time"
+                                                name='time'
+                                                list="listalimitestiempo"
+                                                step="600"
+                                                onChange={handleChange}
+                                                onBlur={handleBlur}
+                                                value={values.time}
+                                            />
+                                            <datalist id="listalimitestiempo">
+                                                <option value="08:00"></option>
+                                                <option value="09:00"></option>
+                                                <option value="10:00"></option>
+                                                <option value="03:00"></option>
+                                            </datalist>
+                                        </div>
+                                    </div>
+                                    <div className="newCita__data">
+                                        <label>Descripción de los sintomas:</label>
+                                        <textarea
+                                            name="description"
+                                            id="descriptio"
+                                            placeholder='Escribe una breve descripción'
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            value={values.description}
+                                        />
+                                    </div>
+                                    {
+                                        message.status && <Message status={data?.Status} message={message} className='login__message' />
+                                    }
+                                    <div className="citas__options" >
+                                        <button
+                                            type='submit'
+                                            className="citas__opcion citas__opcion--background"
+                                        // onClick={handleNewAppointment}
+                                        >Hacer cita</button>
+                                    </div>
+                                </form>
+                            )
                         }
-                        <div className="citas__options" >
-                            <button
-                                className="citas__opcion citas__opcion--background"
-                                onClick={handleNewAppointment}
-                            >Hacer cita</button>
-                        </div>
-                    </form>
+                    </Formik>
                 </div>
             </div>
         </>
